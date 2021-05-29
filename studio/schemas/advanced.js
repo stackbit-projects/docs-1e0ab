66ea6ecd969d9bbfd84f2f2c2db70e0dcc6c9956
Stackbit <projects@stackbit.com>
2021-05-29 06:36:22 +0000

    initial commit by Stackbit

---
+++ studio/schemas/advanced.js
@@ -0,0 +1,81 @@
+export default {
+    "type": "document",
+    "name": "advanced",
+    "title": "Advanced Page",
+    "fields": [
+        {
+            "type": "string",
+            "name": "title",
+            "title": "Title",
+            "description": "The title of the page.",
+            "validation": Rule => Rule.required()
+        },
+        {
+            "type": "array",
+            "name": "sections",
+            "title": "Sections",
+            "description": "Page sections",
+            "validation": null,
+            "of": [
+                {
+                    "type": "section_content"
+                },
+                {
+                    "type": "section_cta"
+                },
+                {
+                    "type": "section_docs"
+                },
+                {
+                    "type": "section_grid"
+                },
+                {
+                    "type": "section_hero"
+                }
+            ]
+        },
+        {
+            "type": "stackbit_page_meta",
+            "name": "seo",
+            "title": "Seo",
+            "validation": null
+        },
+        {
+            "type": "string",
+            "name": "template",
+            "title": "Template",
+            "hidden": false,
+            "validation": Rule => Rule.required(),
+            "options": {
+                "list": [
+                    "advanced"
+                ]
+            }
+        },
+        {
+            "type": "string",
+            "name": "stackbit_url_path",
+            "title": "URL Path",
+            "description": "The URL path of this page relative to site root. For example, the site root page would be \"/\", and post page would be \"posts/new-post/\"",
+            "validation": Rule => Rule.required()
+        },
+        {
+            "type": "string",
+            "name": "stackbit_model_type",
+            "title": "Stackbit Model Type",
+            "description": "Stackbit model type",
+            "hidden": false,
+            "validation": Rule => Rule.required(),
+            "options": {
+                "list": [
+                    "page"
+                ]
+            }
+        }
+    ],
+    "preview": {
+        "select": {
+            "title": "title"
+        }
+    }
+}
